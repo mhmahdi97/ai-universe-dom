db22f62a6df3e472309e1995b0b3e72717ebcd93
mhmahdi97 <mhmahdi97@gmail.com>
2023-05-14 22:48:33 +0600

    implemented all the cards functionality

--- js/app.js
+++ js/app.js
@@ -18,15 +18,17 @@ const displayItems = (items) => {
     itemDiv.innerHTML = `
       <div class="card w-full bg-base-100 shadow-xl">
           <figure>
-            <img class="w-full" src="https://img.olhardigital.com.br/wp-content/uploads/2023/01/chatgpt_assistente.jpg" alt="" />
+            <img class="w-full h-64" src="${item.image}" alt="" />
           </figure>
           <div class="card-body">
             <h2 class="card-title font-bold">Features</h2>
             <!-- Features list here -->
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <p>${item.features[0]}</p>
+            <p>${item.features[1]}</p>
+            <p>${item.features[2]}</p>
             <hr class="border"/>
 
-            <h1 class="text-xl font-bold">ChatGPT</h1>
+            <h1 class="text-xl font-bold">"${item.name}"</h1>
             <div class="flex justify-between">  
               <div class="flex">
                 <span class="mr-1">              
@@ -34,7 +36,7 @@ const displayItems = (items) => {
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
                   </svg>
                 </span>
-                <p>11/01/2022</p>
+                <p>${item.published_in}</p>
   
               </div>
               <div class="bg-red-200 rounded-full p-3 cursor-pointer">
